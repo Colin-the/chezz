5d41d501bf6db47b0e6b80aa2aae08dbb3f62902
Colin <ccampb47@uoguelph.ca>
2025-04-27 03:06:10 +0000

    added the C controller program and gitignore file

--- python/engineController.py
+++ python/engineController.py
@@ -1,13 +1,44 @@
 import sys
 import time
+import numpy as np
 import chezz
 
+def list_to_unsigned_char_array(data_list):
+    try:
+        np_array = np.array(data_list, dtype=np.uint8)
+        return np_array
+    except ValueError:
+        print("Error: Input list could not be converted to a NumPy array.")
+        return None
+  
+exampleBoard = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [11, 11, 11, 11, 11, 11, 11, 11],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+]
+
+board = list_to_unsigned_char_array(exampleBoard)
+src = 0
+dest = 9
+color_to_move = ord('w')
 
 if __name__ == "__main__":
     print("live")
     eval = chezz.getSearchDepth(int(30000),int(950))
 
     print("From C we have ", ord(eval))
+
+    is_valid = chezz.isValidNextMove(board, src, dest, color_to_move)
+
+    if is_valid:
+        print("The move is valid!")
+    else:
+        print("The move is invalid!")
 
     if len(sys.argv) > 2 and sys.argv[2] == "-f":#file option
         #print("Using file")
